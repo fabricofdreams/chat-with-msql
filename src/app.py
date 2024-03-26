6e94a51052c4560dae84b703511fe4981989d834
Fernando Robledo <fernando.robledoruiz@gmail.com>
2024-03-26 13:58:48 -0500

    Add log auth

--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
 import os
-
 from langchain_groq import ChatGroq
 from langchain_community.utilities.sql_database import SQLDatabase
 from langchain_core.messages import AIMessage, HumanMessage
@@ -9,6 +8,7 @@ from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 import streamlit as st
 
+load_dotenv()
 
 mixtral_llm = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768")
 
@@ -80,66 +80,65 @@ def get_response(user_query: str, db: SQLDatabase, chat_history: list):
     return chain.invoke({"chat_history": chat_history, "user_query": user_query})
 
 
-if 'chat_history' not in st.session_state:
-    st.session_state.chat_history = [
-        AIMessage(
-            content="Hi there! I'm a SQL assistant. Ask me anything about your database.")
-    ]
+def run_app():
+    """Run the application."""
 
-load_dotenv()
+    if 'chat_history' not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(
+                content="Hi there! I'm a SQL assistant. Ask me anything about your database.")
+        ]
 
-# Retrieve the database password
-db_password = os.environ.get('DB1_PASSWORD')
-db_user = os.environ.get('DB_USER')
-db_host = os.environ.get('HOST')
-db_port = os.environ.get('PORT')
-db_database = os.environ.get('DATABASE')
+    # Retrieve the database password
+    db_password = os.environ.get('DB1_PASSWORD')
+    db_user = os.environ.get('DB_USER')
+    db_host = os.environ.get('HOST')
+    db_port = os.environ.get('PORT')
+    db_database = os.environ.get('DATABASE')
 
-st.set_page_config(page_title="Chat to MySQL", page_icon=':speech_balloon:')
-st.title(body="Chat to MySQL")
+    with st.sidebar:
 
-with st.sidebar:
-    st.subheader(body="Settings")
-    st.write(
-        "This is an application using MySQL. Connect to the database and start chatting.")
-    st.text_input("User:", value=db_user, key="user")
-    st.text_input("Host:", value=db_host, key="host")
-    st.text_input("Port:", value=db_port, key="port")
-    st.text_input("Password:", type="password",
-                  key="password", value=db_password)
-    st.text_input("Database:", key="database", value=db_database)
+        st.subheader(body="Settings")
+        st.write(
+            "This is an application using MySQL. Connect to the database and start chatting.")
+        st.text_input("User:", value=db_user, key="user")
+        st.text_input("Host:", value=db_host, key="host")
+        st.text_input("Port:", value=db_port, key="port")
+        st.text_input("Password:", type="password",
+                      key="password", value=db_password)
+        st.text_input("Database:", key="database", value=db_database)
 
-    if st.button("Connect"):
-        with st.spinner("Connecting to database..."):
-            db = init_database(
-                user=st.session_state['user'],
-                password=st.session_state['password'],
-                host=st.session_state['host'],
-                port=st.session_state['port'],
-                database=st.session_state['database']
-            )
-            st.session_state.db = db
-            st.success("Connected to database!")
+        if st.button("Connect"):
+            with st.spinner("Connecting to database..."):
+                db = init_database(
+                    user=st.session_state['user'],
+                    password=st.session_state['password'],
+                    host=st.session_state['host'],
+                    port=st.session_state['port'],
+                    database=st.session_state['database']
+                )
+                st.session_state.db = db
+                st.success("Connected to database!")
 
-for message in st.session_state.chat_history:
-    if isinstance(message, AIMessage):
-        with st.chat_message("AI"):
-            st.markdown(message.content)
-    elif isinstance(message, HumanMessage):
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.markdown(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.markdown(message.content)
+
+    user_query = st.chat_input("Type here a message...")
+    if user_query is not None and user_query.strip() != "":
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+
         with st.chat_message("Human"):
-            st.markdown(message.content)
+            st.markdown(user_query)
 
-user_query = st.chat_input("Type here a message...")
-if user_query is not None and user_query.strip() != "":
-    st.session_state.chat_history.append(HumanMessage(content=user_query))
+        with st.spinner("Thinking..."):
+            response = get_response(
+                user_query, st.session_state.db, st.session_state.chat_history)
+            st.session_state.chat_history.append(AIMessage(content=response))
 
-    with st.chat_message("Human"):
-        st.markdown(user_query)
-
-    with st.spinner("Thinking..."):
-        response = get_response(
-            user_query, st.session_state.db, st.session_state.chat_history)
-        st.session_state.chat_history.append(AIMessage(content=response))
-
-    with st.chat_message("AI"):
-        st.markdown(response)
+        with st.chat_message("AI"):
+            st.markdown(response)
---
+++ src/main.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+from app import run_app
+
+st.set_page_config(page_title="Chat to MySQL", page_icon=':speech_balloon:')
+st.title(body="Chat to MySQL")
+
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+authenticator.login()
+
+if st.session_state["authentication_status"]:
+    with st.sidebar:
+        st.image("../FRRcirculo.png", width=75)
+        st.write(f'Welcome *{st.session_state["name"]}*')
+        authenticator.logout()
+
+    # Application
+    run_app()
+
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
